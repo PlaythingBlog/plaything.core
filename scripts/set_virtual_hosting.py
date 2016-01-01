c3aeaa7b44c5b2d904701dfab142553165b7495d
David Bain <david@alteroo.com>
2015-12-31 19:16:09 -0500

    updated initial setup scripts

--- scripts/set_virtual_hosting.py
+++ scripts/set_virtual_hosting.py
@@ -4,5 +4,5 @@ SITE = getenv("SITE_ID")
 HOST='*.herokuapp.com'
 print "-----> Plone site %s exists" % SITE
 print "-----> Setting virtualhost settings"
-app.virtual_hosting.set_map('%s/Plaything' % HOST)
+app.virtual_hosting.set_map('%s/%s' % (HOST,SITE))
 transaction.commit()
